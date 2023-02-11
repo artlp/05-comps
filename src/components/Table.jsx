d1200066c875cbcbbff3b0a3aff2ac104d4e0d16
Artur Lipskii <falcon.arthur@gmail.com>
2023-02-11 20:40:28 +0200

    table page

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 
-function Table({ data }) {
-  const renderedRows = data.map((fruit) => {
-    return (
-      <tr className="border-b" key={fruit.name}>
-        <td className="p-3">{fruit.name}</td>
-        <td className="p-3"><div className={`p-3 m-2 ${fruit.color}`}></div></td>
-        <td className="p-3">{fruit.score}</td>
-      </tr>
-    );
+function Table({ data, config, keyFn }) {
+  const renderedHeaders = config.map((column) => {
+    return <th key={column.label}>{column.label}</th>;
   });
+  const renderedRows = data.map((rowData) => {
+    const renderedCells = config.map((column) => {
+      return <td key={column.label}>{column.render(rowData)}</td>
+    })
+
+    return (
+      <tr className="border-b" key={keyFn(rowData)}>
+      {renderedCells}
+      </tr>
+    ); 
+  });
+
   return (
     <table className="table-auto border-spacing-2">
       <thead>
-        <tr className="border-b-2">
-          <th>Fruit</th>
-          <th>Color</th>
-          <th>Score</th>
-        </tr>
+        <tr className="border-b-2">{renderedHeaders}</tr>
       </thead>
       <tbody>{renderedRows}</tbody>
     </table>
