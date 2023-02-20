962ee5f4428244a29c5ebb9d83c11144cbabd5fe
Artur Lipskii <falcon.arthur@gmail.com>
2023-02-20 20:58:31 +0200

    table done

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,22 +1,28 @@
-import React from "react";
+import { Fragment } from "react";
 
 function Table({ data, config, keyFn }) {
   const renderedHeaders = config.map((column) => {
     if (column.header) {
-      return column.header();
+      return <Fragment key={column.label}>{column.header()}</Fragment>;
     }
+
     return <th key={column.label}>{column.label}</th>;
   });
+
   const renderedRows = data.map((rowData) => {
     const renderedCells = config.map((column) => {
-      return <td key={column.label}>{column.render(rowData)}</td>
-    })
+      return (
+        <td className="p-2" key={column.label}>
+          {column.render(rowData)}
+        </td>
+      );
+    });
 
     return (
       <tr className="border-b" key={keyFn(rowData)}>
-      {renderedCells}
+        {renderedCells}
       </tr>
-    ); 
+    );
   });
 
   return (
--- src/pages/TablePage.jsx
+++ src/pages/TablePage.jsx
@@ -1,27 +1,11 @@
-import SortableTable from "../components/Table";
+import SortableTable from "../components/SortableTable";
 
 function TablePage() {
   const data = [
-    {
-      name: "Orange",
-      color: "bg-orange-500",
-      score: 3,
-    },
-    {
-      name: "Apple",
-      color: "bg-red-500",
-      score: 4,
-    },
-    {
-      name: "Banana",
-      color: "bg-yellow-500",
-      score: 5,
-    },
-    {
-      name: "Lime",
-      color: "bg-green-500",
-      score: 2,
-    },
+    { name: "Orange", color: "bg-orange-500", score: 5 },
+    { name: "Apple", color: "bg-red-500", score: 3 },
+    { name: "Banana", color: "bg-yellow-500", score: 1 },
+    { name: "Lime", color: "bg-green-500", score: 4 },
   ];
 
   const config = [
